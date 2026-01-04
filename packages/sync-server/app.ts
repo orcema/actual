baec00ef39e0f47b633939d1cdf5290839b74f5f
Orce MARINKOVSKI <orce00@gmail.com>
2026-01-04 22:36:12 +0100

    updates to setup container dev env

--- packages/sync-server/app.ts
+++ packages/sync-server/app.ts
@@ -3,7 +3,8 @@ import { run as runMigrations } from './src/migrations';
 runMigrations()
   .then(() => {
     //import the app here becasue initial migrations need to be run first - they are dependencies of the app.js
-    import('./src/app.js').then(app => app.run()); // run the app
+    // Use no extension - loader will resolve to .ts in debug mode, TypeScript will compile to .js in production
+    import('./src/app').then(app => app.run()); // run the app
   })
   .catch(err => {
     console.log('Error starting app:', err);
